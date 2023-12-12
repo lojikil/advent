d40590cda7d7607cbe90dedb7bee60807be6a2dc
Stefan Edwards <sedwards@etsy.com>
2023-12-12 00:19:40 -0500

    eleven part 2 thrash

--- 2023/eleven.py
+++ 2023/eleven.py
@@ -1,6 +1,7 @@
 import sys
 import collections
 import functools
+from dataclasses import dataclass
 
 if len(sys.argv) != 2:
     print("usage: {0} [file]".format(sys.argv[0]))
@@ -49,10 +50,10 @@ def pathfinder(universe, glx, tglx):
 
     probe = glx
     (slope, rise, run) = yeqmxplb(probe, tglx)
-    print("slope of", glx, "=>", tglx, ":", slope)
+    #print("slope of", glx, "=>", tglx, ":", slope)
     cnt = 0
     while True:
-        print(probe, tglx, slope, run, rise)
+        #print(probe, tglx, slope, run, rise)
         if slope > 0.0:
             np = clamped_probe(probe[0] + 1, probe[1], universe)
         elif slope < 0.0:
@@ -70,26 +71,81 @@ def pathfinder(universe, glx, tglx):
         cnt += 1
         (slope, rise, run) = yeqmxplb(probe, tglx)
         if probe == tglx:
-            print("probe == tglx", cnt)
+            #print("probe == tglx", cnt)
             break
     return cnt
+
+def part2(exs, gs):
+    newgs = []
+    for g in gs:
+        y, x = g
+        newy, newx = g
+        for ex in exs:
+            if type(ex) is RowExpand and y >= ex.expansion:
+                print("exanding:", ex, "for:", g)
+                #newy += (1_000_000 - 1)
+                newy += 9
+            elif type(ex) is ColumnExpand and x >= ex.expansion:
+                print("exanding:", ex, "for:", g)
+                #newx += (1_000_000 - 1)
+                newx += 9
+        print("expanded", g, "to", (newy, newx))
+        newgs.append((newy, newx))
+
+    distances = []
+    paths = {}
+    for g in newgs:
+        for tg in newgs:
+            if g == tg:
+                continue
+            elif (g, tg) in paths:
+                continue
+            elif (tg, g) in paths:
+                continue
+            y1, x1 = g
+            y2, x2 = tg
+            print("path for", g, "to", tg)
+            d = abs(x2 - x1) + abs(y2 - y1)
+            paths[(g, tg)] = d
+            distances.append(d)
+    print(len(distances))
+    return sum(distances)
+
+@dataclass
+class Expand:
+    expansion:int
+
+@dataclass
+class RowExpand(Expand):
+    pass
+
+@dataclass
+class ColumnExpand(Expand):
+    pass
 
 universe = []
 galaxies = []
 galactic_paths = {}
+expansions = []
 with open(sys.argv[1]) as fh:
+    cnt = 0
     for line in fh:
         line = line.strip()
         universe.append(line)
         if '#' not in line:
+            expansions.append(RowExpand(cnt))
             universe.append(line)
+        cnt += 1
     # columar expansion is going to be messy
     tempu = transpose(universe)
     lastu = []
+    cnt = 0
     for row in tempu:
         lastu.append(row)
         if '#' not in row:
+            expansions.append(ColumnExpand(cnt))
             lastu.append(row)
+        cnt += 1
     universe = transpose(lastu)
     #print_universe(universe)
     cnt = 0
@@ -118,11 +174,15 @@ with open(sys.argv[1]) as fh:
             elif (target_galaxy, galaxy) in galactic_paths:
                 continue
             p = pathfinder(universe, galaxy, target_galaxy)
-            print("path:", galaxy, target_galaxy, p)
+            #print("path:", galaxy, target_galaxy, p)
             galactic_paths[(galaxy, target_galaxy)] = p
-    sum = 0
+    isum = 0
     cnt = 0
     for path in galactic_paths.values():
-        sum += path
+        isum += path
         cnt += 1
-    print(sum, cnt)
+    #print(isum, cnt)
+    print(expansions)
+    print(part2([], galaxies))
+    # part 2
+    print(part2(expansions, galaxies))
